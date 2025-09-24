4b2888e4023e70a398eeb8b816e55737033f472e
nmrvanbrussel <nmrvanbrussel@gmail.com>
2025-09-24 17:26:53 +0200

    Working on Sentiment anlysis and embeddings pipeline

--- vectorizing_data.py
+++ vectorizing_data.py
@@ -1,3 +1,59 @@
 from transfomers import AutoTokenizer, AutomodelForSequenceClassification, TextClassificationPipeline
 import pandas as pd
+import numpy as np
+from tqdm.auto import tqdm
+import torch
 
+#Change this path to which company you are analyzing
+CSV_PATH = r'C:\Users\nmrva\OneDrive\Desktop\AI_CHIP_STRATEGY\Data\CSV\amd_qna_turns.csv'
+TEXT_COL = 10 
+MODEL_ID = 'ProsusAI/finbert-tone', have to choose which one
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+MAX_LEN = 256 #Could change to 512
+STRIDE = 32 #Lets see what optimizes?
+
+df = pd.read_csv(CSV_PATH)
+
+texts = df[TEXT_COL].astyoe(str).tolist()
+
+tok = AutoTokenizer.from_pretrained(MODEL_ID)
+
+#FinBERT classifier for sentiment Probablities
+#This has a small classification head on top of BERT
+clf = AutoModelForSequenceClassification.from_pretrained(MODEL_ID)
+clf = clf.to(DEVICE).eval()
+
+#FinBERT base model for embeddings
+base = AutoModel.from_pretrained(MODEL_ID)
+base = base.to(DEVICE).eval()
+
+def chunk_encode(text, max_length = MAX_LEN, stride = STRIDE):
+
+    # Step 1 Is to Tokenize without truncation because we want the full token sequence
+    enc = tok(text, return_tensors = "pt", truncation = FALSE)
+
+    # Step 2 Is to unpack the token ID's and attention mask
+    input_ids = enc["input_ids"][0]
+    attention_mask = enc["attention_mask"][0]
+
+    # Step 3 is to slide a window across the sequence, why?
+    chunks = []
+    start = 0 
+    while start < len(input_ids):
+        end = min(start + MAX_LEN, len(input_ids))
+
+        #Slicing the window 
+        ids_slice = input_ids[start:end]
+        am_slice = attention_mask[start:end]
+
+        #Add batch dimension as model expects the tensors to have a batch dimension s.t [B, T]
+        "input_ids": ids_slice.unsqueeze(0),
+        "attention_mask": am_slice.unsqueeze(0)
+
+        chunks.append({
+            "input_ids": ids_slice.unsqueeze(0),
+            "attention_mask": am_slice.unsqueeze(0)
+        })
+        
